12f163b932397121edccb7324e9a2e307811e2a7
Ben Calegari <bcalegari@pivotal.io>
2015-12-15 10:14:16 -0800

    Add initial main method
    [#109766010]
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/cloudfoundry-incubator/cf-mysql-benchmark-app/api"
+	"github.com/cloudfoundry-incubator/cf-mysql-benchmark-app/config"
+	"github.com/cloudfoundry-incubator/cf-mysql-benchmark-app/sysbench_client"
+	"github.com/cloudfoundry-incubator/cf-mysql-benchmark-app/sysbench_client/os_client"
+"fmt"
+)
+
+func main() {
+	rootConfig := config.NewConfig()
+	logger := rootConfig.Logger
+
+	err := rootConfig.ParseEnv()
+	if err != nil {
+		logger.Fatal("Failed to parse environment variables", err)
+	}
+
+	err = rootConfig.Validate()
+	if err != nil {
+		logger.Fatal("Failed to validate config", err)
+	}
+
+	osClient := os_client.New()
+	sysbenchClient := sysbench_client.New(osClient, *rootConfig)
+
+	router, err := api.NewRouter(api.Api{
+		RootConfig:     rootConfig,
+		Routes:         api.DefaultRoutes(),
+		SysbenchClient: sysbenchClient,
+	})
+	if err != nil {
+		logger.Fatal("Failed to create router", err)
+	}
+
+	logger.Info("MySQL Benchmarking is running")
+	err = http.ListenAndServe(fmt.Sprintf(":%d", rootConfig.Port), router)
+	if err != nil {
+		logger.Fatal("MySQL Benchmarking stopped unexpectedly", err)
+	}
+	logger.Info("MySQL Benchmarking has stopped gracefully")
 }
